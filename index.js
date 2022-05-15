7ae029428f13b15d756ecf2071b114b4aecefd8f
partho2589 <parthomalo615@.com>
2022-05-15 23:07:48 +0600

    added booking api

--- index.js
+++ index.js
@@ -19,11 +19,20 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors-portal').collection('service')
+        const bookingCollection = client.db('doctors-portal').collection('booking')
+       
         app.get('/service', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray()
             res.send(services)
+        })
+
+        app.post('/booking', async(req, res)=>{
+            const booking= req.body;
+            const query = {treatment:booking.treatment, date:booking.date, patient:booking.patient}
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result)
         })
 
     }
