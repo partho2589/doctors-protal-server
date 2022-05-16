f31dfbf51533792d1dd2903146a3f08c0f37d981
partho2589 <parthomalo615@.com>
2022-05-16 16:31:54 +0600

    added

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('doctors-portal').collection('service')
         const bookingCollection = client.db('doctors-portal').collection('booking')
+        
        
         app.get('/service', async (req, res) => {
             const query = {}
@@ -29,10 +30,14 @@ async function run() {
         })
 
         app.post('/booking', async(req, res)=>{
-            const booking= req.body;
-            const query = {treatment:booking.treatment, date:booking.date, patient:booking.patient}
-            const result = await bookingCollection.insertOne(booking);
-            res.send(result)
+            const booking = req.body;
+            const query = {treatment :booking.treatment, date:booking.date, patient:booking.patient}
+            const exists = await bookingCollection.findOne(query)
+            if(exists){
+                return res.send({success:false, booking:exists})
+            }
+            const result= await bookingCollection.insertOne(booking)
+           return res.send({success:true, result})
         })
 
     }
